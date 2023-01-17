66b15a5565aa9650f4fc17a81e7cb5103beb352d
tektechnologies <info@tektechnologies.com>
2023-01-16 19:48:12 -0600

    add app class remove () comp

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//1. imports
+import React from 'react';
+
+
+
+//bootstrap form
+//form html
+//class
+//select for form
+//diff between forms
+//event listeners
+//state for user name and sorted data
+//going to filter helper function for selecting even or odd or all numbers. 
+// we have to update state and with forms it can be weird. 
+//helper functions will be for taking in form data, also taking in form input as user is typing it. 
+//2.
+class App extends React.Component {
+
+} // closes our class....
+
+
+
+
+
+
+
+
+
+
+
+//3. 
+export default App;
